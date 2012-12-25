a13cd091aa8cf0b88137e2213c476a58378b6dea
Alon Zakai <alonzakai@gmail.com>
2012-12-24 20:52:29 -0800

    disable lto because of llvm bug 14710

--- doit.sh
+++ doit.sh
@@ -1,4 +1,4 @@
-~/Dev/emscripten/emcc -O2 --closure 0 llvm-as.bc -o llvm-as.pre.js --embed-file llvm.ll -s LABEL_DEBUG=1 -s LIBRARY_DEBUG=1
+~/Dev/emscripten/emcc -O2 --llvm-lto 0 --closure 0 llvm-as.bc -o llvm-as.pre.js --embed-file llvm.ll
 cat shell-pre.js > llvm-as.js
 cat llvm-as.pre.js >> llvm-as.js
 cat shell-post.js >> llvm-as.js
