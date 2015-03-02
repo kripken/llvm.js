0c2afa4bbadb42f30a533e1c07315c2a331e03b0
Stefan Koch <uplink.coder@googlemail.com>
2015-03-02 21:26:17 +0000

    updated build script and llvm-as/dis are on 3.4

--- doit.sh
+++ doit.sh
@@ -1,11 +1,4 @@
 # as
-~/Dev/emscripten/emcc -O2 --llvm-lto 0 --closure 0 llvm-as.bc -o llvm-as.pre.js
-cat shell-as-pre.js > llvm-as.js
-cat llvm-as.pre.js >> llvm-as.js
-cat shell-as-post.js >> llvm-as.js
+../emscripten/emcc -Oz -v --llvm-lto 3 --pre-js shell-as-pre.js --post-js shell-as-post.js llvm-as.bc -o llvm-as.js
 # dis
-~/Dev/emscripten/emcc -O2 --llvm-lto 0 --closure 0 llvm-dis.bc -o llvm-dis.pre.js
-cat shell-dis-pre.js > llvm-dis.js
-cat llvm-dis.pre.js >> llvm-dis.js
-cat shell-dis-post.js >> llvm-dis.js
-
+../emscripten/emcc -Oz -v --llvm-lto 3 --pre-js shell-dis-pre.js --post-js shell-dis-post.js llvm-dis.bc -o llvm-dis.js
