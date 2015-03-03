a601ee71dae553a34915022eae722cd496bf582c
Stefan Koch <UplinkCoder@users.noreply.github.com>
2015-03-03 08:17:21 +0000

    Update doit.sh
    Oh I missed that this won't work when compling with optimisation!

--- doit.sh
+++ doit.sh
@@ -1,4 +1,4 @@
 # as
-../emscripten/emcc -Oz -v --llvm-lto 3 --pre-js shell-as-pre.js --post-js shell-as-post.js llvm-as.bc -o llvm-as.js
+../emscripten/emcc -v --pre-js shell-as-pre.js --post-js shell-as-post.js llvm-as.bc -o llvm-as.js
 # dis
-../emscripten/emcc -Oz -v --llvm-lto 3 --pre-js shell-dis-pre.js --post-js shell-dis-post.js llvm-dis.bc -o llvm-dis.js
+../emscripten/emcc -v --pre-js shell-dis-pre.js --post-js shell-dis-post.js llvm-dis.bc -o llvm-dis.js
